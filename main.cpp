688100adb534ee3c1c8471a9f420629a51048ef9
Crptkc <58362274+Crptkc@users.noreply.github.com>
2023-07-12 17:28:34 +0000

    Finished

--- main.cpp
+++ main.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
 using namespace std;
-//1. Creating make file
-//2. Swap the value in the array using pointers** to help
-//3. Take input from argv
 
+int main(int argc, char **argv)
+{
+    int i, n, *a;
+		n = argc-1;
+ 	  a = new int[n];
+		for(i = 0 ; i < n ; i++) 
+      {
+			    a[i]=atoi(argv[i+1]);
+		      cout<< "a[" << i << "]=" <<a[i]<<endl;
+		  }
 
-int main(int argc, char **argv){
- int i,n,*a;
-			n=argc-1;
- 		     a=new int[n];
-			for(i=0;i<n;i++) {
-			    a[i]=atoi(argv[i]);
-		       cout<< "a[" << i << "]=" <<a[i]<<endl;
-		}
-
-    int*p=a;
-    int*pb;
-
-
+  int *pa = a;
+  int *pb = a+n-1;
+  int temp;
+  cout<< "Original: ";
+  for (int i = 0; i < n ; i++)
+    {
+      cout<<*a+i<<" ";
+    }
+    cout<<endl;
   
-     // Using p within your program 
-  for(i=0;i<n;i++) {
- 		   cout<<p[i]<<" "; //p is the location where p points to
- //     p++;  
- }
+    pa = a; 
+    pb = a+n-1;
+  
+    cout<<"Reverse: ";
+    for (i = 0; i < n / 2; i++) 
+    {
+        temp = *pa;
+        *pa = *pb;
+        *pb = temp;
+        pa++; pb--;
+    }
+    pa = a;
+    for (int i = 0; i < n; i++ , pa++)
+    {
+      cout<<*pa<<" ";
+    }
 
  delete [] a;  
 }
-
-  
-//}
-
